0ee78a12559a803eb0452b0a7db67dbbd16cac4f
vishnugopy <vishnugoby0010@gmail.com>
2021-02-09 20:40:23 +0100

    Login and home fini

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
+
+//histoire du la nav
+let nav = document.querySelector("nav");
+nav.innerHTML= 
+`<img src="./assets/images/SIMPLONNEWS.svg" alt="logo" srcset="">
+
+<div>
+    <i class="barbuton fas fa-bars"></i>
+</div>
+
+<div class="navigation">
+    <a href="">Home</a>
+    <a href="">Log out</a>
+</div>
+
+<div class="navigationphone">
+    <a href="">Home</a>
+    <a href="">Log out</a>
+</div>`;
+
+
 let barbuton = document.querySelector(".barbuton");
 let navigation = document.querySelector(".navigation");
-
 barbuton.addEventListener("click" , () =>{
-    navigation.style.display = "flex";
+
+    if (navigation.className === "navigationhidden") {
+        navigation.classList.add("navigation");
+        navigation.classList.remove("navigationhidden");
+      } else {
+        navigation.classList.add("navigationhidden");
+        navigation.classList.remove("navigation");
+
+      }
+      
 })
+//fin de histoire du la nav
