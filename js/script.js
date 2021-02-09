b173fe6376c05dd43cb878882cde7eaf862bbe1b
vishnugopy <vishnugoby0010@gmail.com>
2021-02-09 17:34:57 +0100

    last push 09

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let barbuton = document.querySelector(".barbuton");
+let navigation = document.querySelector(".navigation");
+
+barbuton.addEventListener("click" , () =>{
+    navigation.style.display = "flex";
+})
