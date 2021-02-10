96e0647e74eb3bdeda528302be1cdea21afcefba
dv-kong <zektbach@outlook.com>
2021-02-10 11:08:04 +0100

    merge

--- js/script.js
+++ js/script.js
@@ -25,4 +25,12 @@ function updateArticlesDisplayed(textInput, displayArea = articlesDisplayArea) {
 }
 
 
+let searchBar = document.querySelector(".searchBar")
+let articlesDisplayArea = document.querySelector("")
+
+searchBar.addEventListener('input', updateArticlesDisplayed)
+
+function updateArticlesDisplayed(textInput, displayArea = articlesDisplayArea) {
+
+}
 // check articles with "textInput" part of str in their name and display then on screen
