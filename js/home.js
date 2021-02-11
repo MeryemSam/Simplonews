ed99573349ae904222de6f3f5701960839f22e6b
dv-kong <zektbach@outlook.com>
2021-02-11 09:44:07 +0100

    com

--- js/home.js
+++ js/home.js
@@ -10,26 +10,26 @@ let fetch_config = {
 
 // Initialise la requete http avec l'url de la ressource et les configurations définis ci-dessus
 fetch("https://simplonews.brianboudrioux.fr/articles", fetch_config)
-.then(function (response) {
-    
-    // On appelle la method json a partir de l'objet response pour parser les données renvoyer par l'API
-    response.json()
-    .then(function (data) {
-        if (response.status == 400) {
-            console.log(data);
-            // gestion erreur données envoyer a la requette
-        } else if (response.status == 403) {
-            console.log(data);
-            // gestion erreur authentification
-        } else {
-            console.log(data);
-            // ici on peut exploiter nos donnée
-            let homesection = document.querySelector(".homesection");
-            let arr = data["articles"];
-            let str = "";
+    .then(function(response) {
+
+        // On appelle la method json a partir de l'objet response pour parser les données renvoyer par l'API
+        response.json()
+            .then(function(data) {
+                if (response.status == 400) {
+                    console.log(data);
+                    // gestion erreur données envoyer a la requette
+                } else if (response.status == 403) {
+                    console.log(data);
+                    // gestion erreur authentification
+                } else {
+                    console.log(data);
+                    // ici on peut exploiter nos donnée
+                    let homesection = document.querySelector(".homesection");
+                    let arr = data["articles"];
+                    let str = "";
                     for (let i = 0; i < arr.length; i++) {
                         str +=
-                        `
+                            `
                         <div class="post" id="${data["articles"].indexOf(arr[i])}">
                         <img src="${data["articles"][i]["img"]}" alt="${data["articles"][i]["title"]} image">
                         <div class="titreandresum">
@@ -39,60 +39,62 @@ fetch("https://simplonews.brianboudrioux.fr/articles", fetch_config)
                         </div>
                         </div>
                         `
+
                     }
 
-
- 
                     homesection.innerHTML = str;
-                    
+
                     let post = document.querySelectorAll(".post");
                     console.log(post);
-                    
+
                     post.forEach(element => {
-                        element.addEventListener("click" , () => {
+                        element.addEventListener("click", () => {
                             var mainid = element.id;
-                            //demain
+                            console.log(mainid);
+
+                            document.location.href = "./article.html";
                             sessionStorage.setItem("passid", mainid);
-                            document.location.href="./article.html"; 
+                            document.location.href = "./article.html";
                         })
-    
+
                     });
                 }
             })
-            .catch(function (data_parsing_error) {
+            .catch(function(data_parsing_error) {
                 console.log(data_parsing_error);
             })
-        })
-        .catch(function (server_errors) {
-            // Cas erreur server (API)
-            console.log(server_errors);
-        })
+    })
+    .catch(function(server_errors) {
+        // Cas erreur server (API)
+        console.log(server_errors);
+    })
 
 
-        
-        window.onload = setupDisconnectFunction()
 
-        function disconnectUserAndRedirectToLoginPage() {
-        
-            sessionStorage.clear()
-            document.location.href = "./index.html";
-        }
-        
-        function setupDisconnectFunction() {
-            let disconnectButton = document.querySelectorAll(".disconnectButton")
-            disconnectButton.forEach(element => {
-                element.addEventListener("click", disconnectUserAndRedirectToLoginPage)
-            });
-        
-        
-        }        
-        function redirectToLoginPage() {
-            let tokenCheck = sessionStorage.getItem("token")
-        
-        
-            if (tokenCheck == null) {
-                disconnectUserAndRedirectToLoginPage()
-            }
-        }
-        
-        redirectToLoginPage();
+window.onload = setupDisconnectFunction()
+
+function disconnectUserAndRedirectToLoginPage() {
+
+    sessionStorage.clear()
+    document.location.href = "./index.html";
+}
+
+function setupDisconnectFunction() {
+    let disconnectButton = document.querySelectorAll(".disconnectButton")
+    disconnectButton.forEach(element => {
+        element.addEventListener("click", disconnectUserAndRedirectToLoginPage)
+    });
+
+
+}
+
+function redirectToLoginPage() {
+    let tokenCheck = sessionStorage.getItem("token")
+
+
+    if (tokenCheck == null) {
+        disconnectUserAndRedirectToLoginPage()
+    }
+}
+
+redirectToLoginPage();
