3e4696b2e30694d4fefb89233df49a768cce0b6c
vishnugopy <vishnugoby0010@gmail.com>
2021-02-11 17:24:40 +0100

    Project Finished 😍

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //histoire du la nav
 let nav = document.querySelector("nav");
 nav.innerHTML= 
-`<a href="home.html"><img src="./assets/images/SIMPLONNEWS.svg" alt="logo" ></a>
+`<a href="home.html"><img src="./assets/images/SIMPLONNEWS.png" alt="logo" ></a>
 
 <div>
     <i class="barbuton fas fa-bars"></i>
