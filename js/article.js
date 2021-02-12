924e0fce308811cbeeee6598b2d5d92911caf5db
dv-kong <zektbach@outlook.com>
2021-02-12 17:34:08 +0100

    need to fix next and previous buttons

--- js/article.js
+++ js/article.js
@@ -1,10 +1,14 @@
 var passid = sessionStorage.getItem("passid");
+
+console.log(passid);
 var token = sessionStorage.getItem("token");
-// console.log(passid);
+
+
 let passIdDuplicate = passid
 
 
 
+// let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTUsImZpcnN0TmFtZSI6bnVsbCwibGFzdE5hbWUiOm51bGwsImVtYWlsIjoib3dvVXNlckB0ZXN0LmZyIiwicGFzc3dvcmQiOiIkMmIkMTAkak45SkhRbmhkUVZ1ekszZndwRGJRZWxMUjg3OU9zeHFSdkR1TXVULlZwdGdHNlZTOTkxWnUiLCJjcmVhdGVkQXQiOiIyMDIxLTAyLTEwVDEzOjMwOjQ4LjAwMFoiLCJ1cGRhdGVkQXQiOiIyMDIxLTAyLTEwVDEzOjMwOjQ4LjAwMFoiLCJpYXQiOjE2MTI5NjM5ODB9.JYF5P8FTHl3UHszkjC613vztA9bCFqKglo6p3P_bW0o";
 let fetch_config = {
     method: "GET",
     headers: {
@@ -17,24 +21,24 @@ let fetch_config = {
 fetch("https://simplonews.brianboudrioux.fr/articles", fetch_config)
     .then(function(response) {
 
-            // On appelle la method json a partir de l'objet response pour parser les données renvoyer par l'API
-            response.json()
-                .then(function(data) {
-                        if (response.status == 400) {
-                            console.log(data);
-                            // gestion erreur données envoyer a la requette
-                        } else if (response.status == 403) {
-                            console.log(data);
-                            // gestion erreur authentification
-                        } else {
-                            console.log(data);
-                            let articlesArray = data["articles"];
+        // On appelle la method json a partir de l'objet response pour parser les données renvoyer par l'API
+        response.json()
+            .then(function(data) {
+                if (response.status == 400) {
+                    console.log(data);
+                    // gestion erreur données envoyer a la requette
+                } else if (response.status == 403) {
+                    console.log(data);
+                    // gestion erreur authentification
+                } else {
+                    console.log(data);
+                    let articlesArray = data["articles"];
 
-                            function generateArticle() {
-                                // ici on peut exploiter nos donnée
-                                let article = document.querySelector(".articlesection");
-                                article.innerHTML =
-                                    `
+                    function generateArticle() {
+                        // ici on peut exploiter nos donnée
+                        let article = document.querySelector(".articlesection");
+                        article.innerHTML =
+                            `
                         <article>
                         <div>
                             <h2 class="articleTitle">${data["articles"][passIdDuplicate]["title"]}</h2>
@@ -47,77 +51,54 @@ fetch("https://simplonews.brianboudrioux.fr/articles", fetch_config)
                             </p>
         
                         </section>
-                        <button class="switchArticleButtons previousArticle">Previous Article</button>
+                        <button class="longbutton previousArticle">Previous Article</a>
                         <a href="home.html" class="longbutton">Accueil</a>
-                        <button class="switchArticleButtons nextArticle">Next Article</button>
+                        <button class="longbutton nextArticle">Next Article</a>
                         
                         </article>
 
                    
                         `
-                                    // NEXT AND PREVIOUS ARTICLE 
-                                let previousArticleButton = document.querySelector(".previousArticle")
-                                let nextArticleButton = document.querySelector(".nextArticle")
+                            // NEXT AND PREVIOUS ARTICLE 
+                        let previousArticleButton = document.querySelector(".previousArticle")
+                        let nextArticleButton = document.querySelector(".nextArticle")
 
-                                function changeCurrentArticleToNext() {
-                                    if (passIdDuplicate < articlesArray.length)
-                                        passIdDuplicate++;
-                                    else {
-                                        passIdDuplicate = 0
-                                    }
-
-                                    console.log(passIdDuplicate);
-                                    generateArticle()
-                                }
-
-
-                                window.onload = setupDisconnectFunction()
-
-                                function disconnectUserAndRedirectToLoginPage() {
-
-                                    sessionStorage.clear()
-                                    document.location.href = "./index.html";
-                                }
-
-                                function setupDisconnectFunction() {
-                                    let disconnectButton = document.querySelectorAll(".disconnectButton")
-                                    disconnectButton.forEach(element => {
-                                        element.addEventListener("click", disconnectUserAndRedirectToLoginPage)
-                                    });
-
-
-                                }
-
-                                function redirectToLoginPage() {
-                                    let tokenCheck = sessionStorage.getItem("token")
-
-
-                                    if (tokenCheck == null) {
-                                        disconnectUserAndRedirectToLoginPage()
-                                    }
-                                }
-
-                                redirectToLoginPage()
-
-
-                                console.log(passIdDuplicate);
-                                generateArticle()
+                        function changeCurrentArticleToNext() {
+                            if (passIdDuplicate < articlesArray.length)
+                                passIdDuplicate++;
+                            else {
+                                passIdDuplicate = 0
                             }
-                            previousArticleButton.addEventListener("click", changeCurrentArticleToPrevious)
-                            nextArticleButton.addEventListener("click", changeCurrentArticleToNext)
 
+                            console.log(passIdDuplicate);
+                            generateArticle()
                         }
-                        generateArticle()
 
+                        function changeCurrentArticleToPrevious() {
+                            if (passIdDuplicate > articlesArray.length)
+                                passIdDuplicate--;
+                            else {
+                                passIdDuplicate = articlesArray.length
+                            }
 
+                            console.log(passIdDuplicate);
+                            generateArticle()
+                        }
+                        previousArticleButton.addEventListener("click", changeCurrentArticleToPrevious)
+                        nextArticleButton.addEventListener("click", changeCurrentArticleToNext)
 
                     }
-                })
-        .catch(function(data_parsing_error) {
-            console.log(data_parsing_error);
-        })
+                    generateArticle()
+
+
+
+                }
+            })
+            .catch(function(data_parsing_error) {
+                console.log(data_parsing_error);
+            })
     })
-.catch(function(server_errors) {
-    // Cas erreur server (API)
-    console.log(server_errors);
-})
+    .catch(function(server_errors) {
+        // Cas erreur server (API)
+        console.log(server_errors);
+    })
